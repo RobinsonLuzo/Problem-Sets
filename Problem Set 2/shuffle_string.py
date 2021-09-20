564cb8266605c8093d8e01dcdc7f4f3c92972e49
RobinsonLuzo <bendanielfagan@gmail.com>
2021-09-20 08:32:31 +0100

    String shuffle problem added. Created new problem set to be added to.

---
+++ Problem Set 2/shuffle_string.py
@@ -0,0 +1,20 @@
+from typing import List
+import unittest
+
+def restore_string(s: str, indicies: List[int]) -> str:
+    """
+    Given a string s and indicies, shuffle the string until the indicies 
+    (and thus characters) are ordered correctly by index. I.e from smallest to biggest number.
+    """
+    answer = ""
+
+    for i in range(len(indicies)):
+        answer += s[indicies.index(i)]
+        print(indicies.index(i), indicies[i])
+    
+    return answer
+
+
+class TestCuboid(unittest.TestCase):
+    def test_volume(self):
+        self.assertEqual(restore_string("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]), "leetcode")
