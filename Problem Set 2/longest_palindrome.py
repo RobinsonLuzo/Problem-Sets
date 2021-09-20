c8e6ef1bbdd8926717c7cbfb0c9f072a4159642e
RobinsonLuzo <bendanielfagan@gmail.com>
2021-09-20 10:30:23 +0100

    Algorithm for finding the longest palindrome in a string. Created 2 test cases.

---
+++ Problem Set 2/longest_palindrome.py
@@ -0,0 +1,39 @@
+from collections import Counter
+import unittest
+
+# Leetcode 409: Longest Palindrome
+
+
+def longest_palindrome(s: str) -> int:
+    """
+    Given a string of both upper and lowercase letters, 
+    return the longest case-sensitive palindrome that can be made.
+
+    The trick for this is counting the number of occurences of each character. 
+    If the count if even for that char then it can be used. 
+    If there is an odd number then we just add 1 to the final result.
+    """
+    final_count = 0
+    odd_is_present = False # flag to indicate if an odd number has occured for final total.
+    char_count = Counter(s)
+
+    # check character occurences and add to final total if valid.
+    for char in char_count:
+        if char_count[char] % 2 == 0:
+            final_count += char_count[char]
+        else:
+            odd_is_present = True
+
+    # If there is at least 1 odd number increment final result by 1
+    if odd_is_present == True:
+        return final_count + 1
+    else:
+        return final_count
+
+
+# Test cases:
+class TestLongestPalin(unittest.TestCase):
+    def test_longest_palindrome(self):
+        self.assertEqual(longest_palindrome("abccccdd"),
+                         7)  # Should be: "dccaccd"
+        self.assertEqual(longest_palindrome("s"), 1)
